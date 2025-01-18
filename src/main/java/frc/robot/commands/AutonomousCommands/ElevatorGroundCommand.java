8ae6a0748738042fde85070798fb04d45cae88b2
AidricRobinson <Aidricrob@gmail.com>
2025-01-17 16:30:56 -1000

    Elevator subsystem and commands
    This adds the elevator subsystem and both the manual and auto commands messurements are placehoders

---
+++ src/main/java/frc/robot/commands/AutonomousCommands/ElevatorGroundCommand.java
@@ -0,0 +1,33 @@
+package frc.robot.commands.AutonomousCommands;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ElevatorSubsystem;
+
+public class ElevatorGroundCommand extends Command{
+    
+    private ElevatorSubsystem m_elevatorSubsystem;
+    private GenericHID controller;
+
+    public ElevatorGroundCommand(ElevatorSubsystem elevatorSubsystem, GenericHID controller){
+        m_elevatorSubsystem = elevatorSubsystem;
+        this.controller = controller;
+    }
+
+    @Override
+    public void initialize(){
+    }
+
+    @Override
+    public void execute(){
+        m_elevatorSubsystem.setPower(m_elevatorSubsystem.getOutput(?????));
+    }
+    @Override
+    public void end(boolean interupted){
+        m_elevatorSubsystem.shutdown();
+    }
+    @Override
+    public boolean isFinished(){
+        return m_elevatorSubsystem.getCurrentError() == 0;
+    }
+}
